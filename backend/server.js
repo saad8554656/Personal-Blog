0c4dd7fdbaa28c67045a62dd83f389e2f95f6034
saad8554656 <ks136647@gmail.com>
2025-05-09 17:11:58 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['https://personal-blog-mern.netlify.app/', 'http://localhost:3000'] 
-    : '*',
+  origin: ['https://personal-blog-mern.netlify.app/', 'http://localhost:3000'] ,
   credentials: true
 }));
 app.use(express.json());
